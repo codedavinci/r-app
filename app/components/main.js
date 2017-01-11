d282fb7bccd501b7eeeb6c8ffcb9670bfbe38ec3
Eddie Henrick <codedavinci.now@gmail.com>
2017-01-11 02:18:21 -0500

    added a home component

--- app/components/main.js
+++ app/components/main.js
@@ -1,13 +1,10 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-var Main = React.createClass({
-    render: function(){
+var Main = () => {
         return (
             <div> Hello World </div>
-        )
-    }
-    
-});
+        )  
+}
 
-ReactDOM.render(<Main />, document.getElement('app'));
+module.exports = Main;
